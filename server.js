77db0631e2812f35ddbfaca32ec01c249254bc88
Daniel Carazo <danielcarazo20@gmail.com>
2021-08-17 20:08:54 -0400

    renamed route foler, added handlebar dependencies, created html template

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes 
 app.use(routes);
 
 // turn on connection to db and server 
                 // force: true/false similar to DROP TABLE IF EXISTS
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
